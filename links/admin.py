7166b016b436d06c82e6ea5c3657eb04d7e6cded
bekirbali <bfbali43@gmail.com>
2026-09-25 18:16:48 +0300

    chore: untrack db.sqlite3 from git and register Link in admin

--- links/admin.py
+++ links/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Link
 
-# Register your models here.
+@admin.register(Link)
+class LinkAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'type', 'is_clean', 'watched_at', 'created_at')
+    list_filter = ('is_clean', 'type', 'created_at', 'watched_at')
+    search_fields = ('title', 'url', 'video_id', 'user__email')
+    ordering = ('-created_at',)
